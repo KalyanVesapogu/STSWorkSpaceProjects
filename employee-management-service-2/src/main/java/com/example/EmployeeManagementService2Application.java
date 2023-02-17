bee23447b005e4afea5c44f624efb482377c89da
KalyanVesapogu <101237290+KalyanVesapogu@users.noreply.github.com>
2023-02-18 00:04:48 +0530

    Add files via upload

---
+++ employee-management-service-2/src/main/java/com/example/EmployeeManagementService2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementService2Application.class, args);
+	}
+
+}
